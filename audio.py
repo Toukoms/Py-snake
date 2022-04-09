d129c0b3a5982726ee3c678b7b9c3aa548be38e4
Toukoms <tokiniaina090605@gmail.com>
2022-04-09 11:03:50 +0300

    Jeu de serpent avec pygame

---
+++ audio.py
@@ -0,0 +1,16 @@
+from asyncore import loop
+from pygame import mixer
+
+class AudioManager:
+
+    def __init__(self):
+        self.sounds = {
+            'nourriture' : mixer.Sound("assets/Serpent_NOURRITURE.wav"),
+            'perdu' : mixer.Sound("assets/Serpent_PERDU.wav"),
+            'music' : mixer.Sound("assets/music.wav")
+        }
+
+    def play(self,name_audio):
+        self.sounds[name_audio].play().set_volume(0.25)
+        if name_audio == "music":
+            self.sounds[name_audio].play(-1).set_volume(0.75)
---
+++ main.py
@@ -0,0 +1,89 @@
+from player import Game  
+from pygame import display, draw, event, init,time, font
+from pygame.locals import *
+
+# Initialisation de pygame
+init()
+
+# Les variables
+COL = 15
+LIN = 20
+PIX = 32
+game = Game()
+TIMER = USEREVENT
+time.set_timer(TIMER, 200)
+
+def afficher_ligne():
+    for x in range(0,COL):
+        for y in range(0, LIN):
+            draw.rect(fenetre, (0,0,0), (x*PIX,y*PIX,PIX,PIX), width=1)
+
+n = 2
+# Création de fenêtre
+fenetre = display.set_mode((COL*PIX,LIN*PIX))
+display.set_caption(f"Snake2 {n}")
+# font de texte à affiche
+font_text = font.SysFont("monospace", 18)
+font_game_over = font.SysFont("Ravie", 30)
+# Texte à affiche
+text_game_over = font_game_over.render("Game Over", 1, (255,25,25))
+pause = font_text.render("Appuyer sur ESPACE pour continuer...", 1, (0,0,0))
+
+# Le boucle principale (Mainloop)
+Run = True
+while Run:
+    
+    # La couleur de fenêtre (Blanc)
+    fenetre.fill((0,255,20))
+
+    # dessiner les elements de jeu
+    game.draw_element(fenetre)
+
+    # Quand le jeu est en pause ou game_over
+    if game.is_playing == False:
+        fenetre.blit(pause, (2*PIX, 9*PIX))
+        
+    if game.over:
+        fenetre.blit(text_game_over, (5*PIX,3*PIX))
+
+    # Pour le maket on affiche la ligne et la colone
+    # afficher_ligne()
+
+    # Gestion d'évenement
+    for e in event.get():
+
+        if e.type == QUIT:
+            if game.score > game.score_max:
+                game.ecrire_maxscore(game.score)
+            Run = False
+        
+        if e.type == KEYDOWN:
+            
+            if game.is_playing:
+                if e.key == K_RIGHT and game.snake.direction != "gauche" :
+                    game.snake.direction = "droite"
+                    break
+                elif e.key == K_LEFT and game.snake.direction != "droite" :
+                    game.snake.direction = "gauche"
+                    break
+                elif e.key == K_DOWN and game.snake.direction != "haut" :
+                    game.snake.direction = "bas"
+                    break
+                elif e.key == K_UP and game.snake.direction != "bas" :
+                    game.snake.direction = "haut"
+                    break
+
+            elif e.key == K_SPACE:
+                game.is_playing = True
+                game.over = False
+            
+        elif e.type == TIMER:
+            display.set_caption(f"Snake2                                Score max : {game.score_max} | Score : {game.score}")
+            if game.is_playing == True:
+                    game.update()
+            game.game_over(COL,LIN)
+
+    # Actualisation de fenêtre
+    display.flip()
+
+quit()
